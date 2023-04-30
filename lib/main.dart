8f381cfb301c3c24be9bdf2fbb1445a82c7c82b2
asamoah-fred <asamoahgodfred42@gmail.com>
2023-04-30 09:40:53 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const Pamital());
+}
+
+class Pamital extends StatelessWidget {
+  const Pamital({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NavigationMenu(index: 0),
+    );
+  }
+}
